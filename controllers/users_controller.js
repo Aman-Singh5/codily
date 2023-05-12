147891b70d27a31949cf3a124acedb7e27feb73d
Aman Singh <amansinghedu123@gmail.com>
2023-05-12 23:00:11 +0530

    crete the signup and signin ejs files

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,27 @@ module.exports.profile = function(req, res){
         title: "hello there"
     });
 }
+
+//render the signup page
+module.exports.signup = function(req, res){
+    return res.render('user_signup',{
+        title: "Codial SignUp"
+    });
+}
+
+//render the signin page
+module.exports.signin = function(req, res){
+    return res.render('user_signin',{
+        title: "Codial Signin"
+    });
+}
+
+// get the signup data
+module.exports.create = function(req, res){
+    //todo later
+}
+
+//sign in and create the session for the user.
+module.exports.createSession = function(req, res){
+    //todo later
+}
